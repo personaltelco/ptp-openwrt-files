2828ae904b0916dacec1f841f6e5eaf60d9056ae
Russell Senior <russell@personaltelco.net>
2009-06-29 19:17:32 -0700

    added www tree, basically functional now?

--- FOOCAB.pl
+++ FOOCAB.pl
@@ -20,6 +20,7 @@ chomp $header;
 my $hi = undef;
 my $ni = undef;
 my $di = undef; # index of DHCPSTART
+my $li = undef; # index of LOGOFILE
 
 for ($i = 0 ; $i < @vars ; $i++)
 {
@@ -35,7 +36,13 @@ for ($i = 0 ; $i < @vars ; $i++)
     {
 	$di = $i;
     }
+    elsif ($vars[$i] eq "LOGOFILE")
+    {
+	$li = $i;
+    }
 }
+
+my $logo = undef;
 
 while(<NODEDB>) {
     chomp;
@@ -61,6 +68,9 @@ while(<NODEDB>) {
 	    if ($vars[$i] eq "LOCALADDR") {
 		$localaddr = $vals[$i];
 	    }
+	    if ($vars[$i] eq "LOGOFILE") {
+		$logo = $vals[$i];
+	    }
 	}
 	
 	(defined $masklen && defined $localaddr) || die "Not enough information to compute network!";
@@ -72,7 +82,6 @@ while(<NODEDB>) {
 
 	print SED "s/PTP_LOCALNET_PTP/$netaddr/g\n";
 	print SED "s/PTP_LOCALNETMASK_PTP/$mask/g\n";
-
     }
 }
 
@@ -86,7 +95,7 @@ while(<FILES>) {
     my $outdir = join('/',"output",@path);
     my $dest = join('/',"output",@path,$fname);
 
-    print "source = $src ; outdir = $outdir ; dest = $dest\n";
+    # print "source = $src ; outdir = $outdir ; dest = $dest\n";
 
     ($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,$atime,$mtime,$ctime,$blksize,$blocks) = stat($src);
 
@@ -96,4 +105,33 @@ while(<FILES>) {
 
     chmod($mode,$dest);
     chown($uid,$gid,$dest);
+}
+
+open(WWW,"find www -type f | grep -v nodes |");
+
+while(<WWW>) {
+    chomp;
+    my $src = $_;
+    my @path = split('/',$src);
+    my $fname = pop @path;
+    my $outdir = join('/',"output",@path);
+    my $dest = join('/',"output",@path,$fname);
+
+    # print "source = $src ; outdir = $outdir ; dest = $dest\n";
+
+    ($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,$atime,$mtime,$ctime,$blksize,$blocks)
+	= stat($src);
+
+    unless (-d $outdir) { system("mkdir -p $outdir"); }
+
+    system("sed -f foocab.sed < $src > $dest");
+
+    chmod($mode,$dest);
+    chown($uid,$gid,$dest);
+}
+
+if (defined($logo) && $logo ne "") {
+    my $src = "www/images/node/$logo";
+
+    system("cp -p $src output/www/images/$logo");
 }
